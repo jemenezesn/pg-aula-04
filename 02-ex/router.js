c43316e1bcb0d225a6f4d79943fff22de15899ff
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-25 18:37:10 -0300

    Implementaçao de rotas put e delete

--- 02-ex/router.js
+++ 02-ex/router.js
@@ -46,6 +46,29 @@ router.post('/', (req, res) => {
   return res.status(200).json(users);
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
 
+  const user = users.find(user => user.id === Number(id));
+
+  if(!user) return res.status(400).json({ error: 'Usuário não encontrado'});
+
+  users = users.map(user => user.id === Number(id) ? {id: Number(id), ...body} : user);
+
+  return res.status(200).json(users);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const user = users.findIndex(user => user.id === Number(id));
+  if(user === -1) return res.status(400).json({ error: 'Usuário não encontrado'});
+  
+  const deletedUser = users.splice(user, 1);
+  console.log(deletedUser);
+
+  return res.status(200).json(users);
+});
 
 module.exports = router;
