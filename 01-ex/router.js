c75f6138fd3ffbe6f161171c7b72e880b0e95c27
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-25 17:51:24 -0300

    Implementaçao de rotas get

--- 01-ex/router.js
+++ 01-ex/router.js
@@ -3,6 +3,22 @@ const express = require('express');
 const router = express.Router();
 let products = require('./products');
 
+router.get('/', (req, res) => {
+  return res.json(products);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const product = products.find(product => product.id === Number(id));
+  
+  if(!product) {
+    return res.status(404).json({ error: 'Produto não encontrado' });
+  }
+
+  return res.status(200).json(product);
+});
+
 
 
 module.exports = router;
