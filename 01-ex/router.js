5a00f627446a2e904529617934a73fe8e29822cb
Jessica Menezes <jessica.menezes@mercadolivre.com>
2022-07-25 17:51:52 -0300

    Implementaçao de rotas post, put e delete

--- 01-ex/router.js
+++ 01-ex/router.js
@@ -19,6 +19,42 @@ router.get('/:id', (req, res) => {
   return res.status(200).json(product);
 });
 
+router.post('/', (req, res) => {
+  const product = req.body;
 
+  products = [...products, {id: products.length + 1, ...product }];
+
+  return res.status(200).json(products);
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+
+  const product = products.find(product => product.id === Number(id));
+
+  if(!product) {
+    return res.status(404).json({ error: 'Produto não encontrado' });
+  }
+
+
+  products = products.map(prod => prod.id === Number(id) ? {id: Number(id), ...body} : prod);
+
+  return res.status(200).json(products);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const index = products.findIndex(product => product.id === Number(id));
+
+  if(index === -1) {
+    return res.status(404).json({ error:  'Produto não encontrado' });
+  }
+
+  const deletedProduct = products.splice(index, 1);
+  console.log(deletedProduct)
+  return res.status(200).json(products);
+});
 
 module.exports = router;
